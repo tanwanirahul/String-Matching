1b49fdccfd0de1aa938c7df1dab8ba42084fca53
tanwairahul <tanwanirahul@gmail.com>
2014-11-15 14:36:20 +0530

    Adding a placeholder implementation for Trie

---
+++ src/com/test/stringmatch/lib/STrie.java
@@ -0,0 +1,31 @@
+package com.test.stringmatch.lib;
+
+/**
+ * Our custom implementation of Trie.
+ */
+public class STrie
+{
+    /**
+     * Adds a given string value into a Trie.
+     */
+    public void add(String value)
+    {
+        
+    }
+
+    /**
+     * For a given string, find a longest prefix match.
+     */
+    public String getLongestPrefixValue(String value)
+    {
+        return null;
+    }
+
+    /**
+     * For a given string, returns the value of the exact match.
+     */
+    public String getExactStringValue(String value)
+    {
+        return null;
+    }
+}
