3006ad7ef9e9888c9c762b1122a147ede618303f
tanwairahul <tanwanirahul@gmail.com>
2014-11-15 16:26:18 +0530

    Formatting StringMatcher

--- src/com/test/stringmatch/StringMatcher.java
+++ src/com/test/stringmatch/StringMatcher.java
@@ -1,45 +1,69 @@
 package com.test.stringmatch;
 
+import com.test.stringmatch.lib.STrie;
+
+
 public class StringMatcher
 {
-/// Add an exact string match, i.e. ÒabcÓ in 
-/// the documentation above. Adding an exact match for an
-/// existing `exact_str` will overwrite the previous `id`.
-/// @param exact_str string to match with the id.
-/// @param id(>=0) that is mapped to this string.
-public void add_exact_match(String exact_str, int id) {
-}
 
-/// Add a prefix string match i.e. ÒfasÓ in
-/// the documentation above. Adding a prefix match for an
-/// existing `prefix_str` will overwrite the previous `id`.
-/// @param prefix_str to match with the id.
-/// @param id(>=0) that is mapped to this string.
-public void add_prefix_match(String prefix_str, int id) {
-}
+    private STrie trie;
 
-/// Get the id for the specified string.
-/// @param input to lookup the id for
-/// @returns -1 if there is no match or the id
-public int lookup(String input) {
-	return 0;
-}
 
-/// Delete the exact matches for the given string, i.e. if we have
-/// an add_exact_match(ÒstrÓ, 2), delete_exact_match will remove the
-/// match of ÒstrÓ to 2.
-/// @param exact_str exact match to delete.
-/// @return true if a match was deleted.
-boolean delete_exact_match(String exact_str) {
-	return false;
-}
+    public StringMatcher()
+    {
+        trie = new STrie();
+    }
 
-/// Delete the prefix matches for the given string, i.e. if we have
-/// a add_prefix_match(ÒstrÓ, 2), delete_prefix_match will remove the
-/// match of ÒstrÓ to 2.
-/// @param prefix_str prefix match to delete.
-/// @return true if a match was deleted. 
-boolean delete_prefix_match(String prefix_str) {
-	return false;
-}
-}; 
+
+    // / Add an exact string match, i.e. ï¿½abcï¿½ in
+    // / the documentation above. Adding an exact match for an
+    // / existing `exact_str` will overwrite the previous `id`.
+    // / @param exact_str string to match with the id.
+    // / @param id(>=0) that is mapped to this string.
+    public void add_exact_match(String exact_str, int id)
+    {
+        trie.add(exact_str, id);
+    }
+
+
+    // / Add a prefix string match i.e. ï¿½fasï¿½ in
+    // / the documentation above. Adding a prefix match for an
+    // / existing `prefix_str` will overwrite the previous `id`.
+    // / @param prefix_str to match with the id.
+    // / @param id(>=0) that is mapped to this string.
+    public void add_prefix_match(String prefix_str, int id)
+    {
+        trie.addPrefix(prefix_str, id);
+    }
+
+
+    // / Get the id for the specified string.
+    // / @param input to lookup the id for
+    // / @returns -1 if there is no match or the id
+    public int lookup(String input)
+    {
+        return trie.lookup(input);
+    }
+
+
+    // / Delete the exact matches for the given string, i.e. if we have
+    // / an add_exact_match(ï¿½strï¿½, 2), delete_exact_match will remove the
+    // / match of ï¿½strï¿½ to 2.
+    // / @param exact_str exact match to delete.
+    // / @return true if a match was deleted.
+    boolean delete_exact_match(String exact_str)
+    {
+        return trie.removeComplete(exact_str);
+    }
+
+
+    // / Delete the prefix matches for the given string, i.e. if we have
+    // / a add_prefix_match(ï¿½strï¿½, 2), delete_prefix_match will remove the
+    // / match of ï¿½strï¿½ to 2.
+    // / @param prefix_str prefix match to delete.
+    // / @return true if a match was deleted.
+    boolean delete_prefix_match(String prefix_str)
+    {
+        return trie.removePrefix(prefix_str);
+    }
+};
